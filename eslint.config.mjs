501b5d0601d4ada45dd325fa2a4902395fa391d3
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2025-02-07 12:29:31 +0000

    PP-13499: Add stylistic eslint rules with recommended config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
   eslint.configs.recommended,
+  stylistic.configs['recommended-flat'],
   tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
